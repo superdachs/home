e36b7f0b86468779ef195551ca18483264d4bb47
Stefan Kauerauf <superdachs@pandora.fritz.box>
2016-08-03 22:42:11 +0200

    emacs

--- .emacs.d/elpa/bts-github-20150108.27/bts-github-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bts-github" "20150108.27" "A plugin of bts.el for GitHub" '((bts "0.0.1") (gh "0.8.2")) :url "https://github.com/aki2o/emacs-bts-github" :keywords '("convenience"))
--- .emacs.d/elpa/dirtree-20140129.32/dirtree-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dirtree-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dirtree.el") (22085 61767 593043 793000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dirtree-autoloads.el ends here
