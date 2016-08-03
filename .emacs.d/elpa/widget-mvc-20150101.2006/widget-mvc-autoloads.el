6bf9129a9d5ee8854aea8bb16939f24d0cb744cc
Stefan Kauerauf <superdachs@pandora.fritz.box>
2016-08-03 22:41:41 +0200

    emacs

--- .emacs.d/elpa/widget-mvc-20150101.2006/widget-mvc-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; widget-mvc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("widget-mvc.el") (22085 61771 552499 969000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; widget-mvc-autoloads.el ends here
--- .emacs.d/elpa/widget-mvc-20150101.2006/widget-mvc-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "widget-mvc" "20150101.2006" "MVC framework for the emacs widgets" 'nil :keywords '("lisp" "widget"))
