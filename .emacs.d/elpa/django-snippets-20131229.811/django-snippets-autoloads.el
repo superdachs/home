18e15ded8e2fb0f4ad9a6918c813ffebfeb643c9
Stefan Kauerauf <superdachs@pandora.fritz.box>
2016-08-03 22:41:07 +0200

    emacs

--- .emacs.d/elpa/django-snippets-20131229.811/django-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; django-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "django-snippets" "django-snippets.el" (22085
-;;;;;;  61765 53248 172000))
-;;; Generated autoloads from django-snippets.el
-
-(autoload 'django-snippets-initialize "django-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(django-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("django-snippets-pkg.el") (22085 61765
-;;;;;;  65263 915000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; django-snippets-autoloads.el ends here
