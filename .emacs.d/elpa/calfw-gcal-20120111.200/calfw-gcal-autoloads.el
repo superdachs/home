31e9bedaf001b45fdf84557a47bc4c00726370e3
Stefan Kauerauf <superdachs@pandora.fritz.box>
2016-08-03 22:40:26 +0200

    emacs

--- .emacs.d/elpa/calfw-gcal-20120111.200/calfw-gcal-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; calfw-gcal-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("calfw-gcal-pkg.el" "calfw-gcal.el") (22085
-;;;;;;  63547 885185 592000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; calfw-gcal-autoloads.el ends here
--- .emacs.d/elpa/color-theme-github-0.0.3/color-theme-github-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; color-theme-github-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("color-theme-github.el") (22085 58845
-;;;;;;  958313 526000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-github-autoloads.el ends here
